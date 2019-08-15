34cf853a1bb7b8e6870c2a69bfd7ace099e02cce
miya6986 <miyadev49802@gmail.com>
2019-08-15 20:47:35 +0900

    Change root path
    - Change root path 'message#index' to 'groups#index'
    - Add route groups#index
    -  Make groups/index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   
-  root 'messages#index'
-  resources :users, only: [:index, :edit, :update]
-  resources :groups, only: [:edit, :new, :create, :update] do
-    resources :messages, only:[:index, :create]
+  root 'groups#index'
+  resources :users, only: [:edit, :update]
+  resources :groups, only: [:index, :edit, :new, :create, :update] do
+    resources :messages, only: [:index, :create]
   end
 
 end
