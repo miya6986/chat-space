a44111f33034ab8d2fa4417dc2f85b809ec028dc
miya6986 <miyadev49802@gmail.com>
2019-08-15 15:54:39 +0900

    Add fuction to make group
    - Add function to make group
    - Add error message output
    - DB recognition error fix

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ApplicationRecord
   has_many :group_users
   has_many :users, through: :group_users
-  validate :title, presence: true, uniqueness: true
+  validates :title, presence: true, uniqueness: true
 end
