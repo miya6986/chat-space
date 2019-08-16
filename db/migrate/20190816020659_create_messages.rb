6fe0139317b961fc20f62763621b4a1c6cfb4ed2
miya6986 <miyadev49802@gmail.com>
2019-08-16 12:07:01 +0900

    Migration fix

--- db/migrate/20190816020659_create_messages.rb
+++ db/migrate/20190816020659_create_messages.rb
@@ -4,7 +4,8 @@ class CreateMessages < ActiveRecord::Migration[5.0]
       t.string  :content
       t.string  :image
       t.references  :group, foreign_key: true
-      t.timestamps  :user, foreign_key: true
+      t.references  :user, foreign_key: true
+      t.timestamps
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -32,9 +32,11 @@ ActiveRecord::Schema.define(version: 20190816020659) do
     t.string   "content"
     t.string   "image"
     t.integer  "group_id"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["group_id"], name: "index_messages_on_group_id", using: :btree
+    t.index ["user_id"], name: "index_messages_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -54,4 +56,5 @@ ActiveRecord::Schema.define(version: 20190816020659) do
   add_foreign_key "group_users", "groups"
   add_foreign_key "group_users", "users"
   add_foreign_key "messages", "groups"
+  add_foreign_key "messages", "users"
 end
