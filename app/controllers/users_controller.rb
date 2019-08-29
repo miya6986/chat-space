e5ab22d11f34070146ff45a99bd5bfc92b876b89
miya6986 <miyadev49802@gmail.com>
2019-08-29 14:53:24 +0900

    For adding user into group
    - Correct groups_controller
    - Correct users_controller
    - Correct user.js
    - Correct  groups/_group.html

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.where("name LIKE ?", "%#{search_params[:keyword]}%").where.not(id: current_user.id)
+    @users = User.where('name LIKE(?) and id NOT IN (?)', "%#{search_params[:keyword]}%", excluded_users)
     respond_to do |format|
       format.html { redirect_to root_path }
       format.json
@@ -25,7 +25,18 @@ class UsersController < ApplicationController
   end
 
   def search_params
-    params.permit(:keyword)
+    params.permit(:keyword, :user_ids)
+  end
+
+  def excluded_users
+    excluded_users = []
+    excluded_users << current_user.id
+    if params[:user_ids]
+      params[:user_ids].map do |user_id|
+        excluded_users << user_id.to_i
+      end
+    end
+    return excluded_users
   end
 
 end
