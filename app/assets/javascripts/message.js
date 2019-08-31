3813e07bf07957fb506564a9319b7fa30f21da55
miya6986 <miyadev49802@gmail.com>
2019-08-31 18:38:45 +0900

    Imprement message auto reload
    - Correct message.js
    - Make api/message_controller
    - Correct messages/create.json.jbuilder
    - Make api/messages/index.json.jbuilder
    - routes fix

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,8 @@
 $(function() {
-
-  function buildHTML(message) {
+  
+  var buildHTML = function(message) {
     image = ( message.image == null ? "": `<img src="${ message.image }" width="300" height="300">` );
-    var html = `<div class="chat-contents">
+    var html = `<div class="chat-contents" data-message-id="${ message.id }">
                   <div class="chat-user">
                     <div class="chat-user__name">
                       ${ message.user_name }
@@ -22,6 +22,47 @@ $(function() {
                 </div>`
     return html;
   }
+
+  var builedMessageHTML = function(message) {
+      var message_content = ( message.content == null ? "": `<div class="chat-contents__message"><p>${ message.content }</p></div>`);
+      var message_image = ( message.image == null ? "": `<img src="${ message.image }" width="300" height="300">`);
+      var html = `<div class="chat-contents" data-message-id="${ message.id }">
+                    <div class="chat-user">
+                      <div class="chat-user__name">
+                        ${ message.user_name }
+                      </div>
+                      <div class="chat-user__date">
+                        <p>
+                          ${ message.time }
+                        </p>
+                      </div>
+                    </div>
+                    ${ message_content }
+                    ${ message_image }
+                  </div>`
+      return html;
+    }
+
+  var reloadMessages = (function() {
+    last_message_id = $(".chat-contents:last").data("message-id");
+    $.ajax({
+      type: 'GET',
+      url: 'api/messages',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      var html = "";
+      $.each(messages, function(i, message) {
+        html = builedMessageHTML(message);
+        $(".chat-contents-wrapper").append(html);
+      })
+      $(".chat-contents-wrapper").animate({scrollTop: $(".chat-contents-wrapper")[0].scrollHeight});
+    })
+    .fail(function() {
+      alert("error");
+    })
+  })
 
   $(".chat-form").on("submit", function(e) {
     e.preventDefault();
@@ -47,4 +88,7 @@ $(function() {
       $('.chat-form__btn').prop("disabled", false);
     })
   })
+  if (/\/groups\/\d*\/messages/.test(window.location.href)) {
+  setInterval(reloadMessages, 5000);
+  }
 })
