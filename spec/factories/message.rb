1c95e3bc398bc64a57a144e73b3c13089494dc51
miya6986 <miyadev49802@gmail.com>
2019-09-03 21:17:34 +0900

    Upload test image

--- spec/factories/message.rb
+++ spec/factories/message.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :message do
     content {Faker::Invoice.reference}
-    image {File.open("#{Rails.root}/public/uploads/message/image/1/beach-2213623_960_720.jpg")}
+    image {File.open("#{Rails.root}/spec/fixture/image/beach-2213623_960_720.jpg")}
     user
     group
   end
